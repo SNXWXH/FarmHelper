63cada19d76ac030f3322e0b6b93ddfc21447da8
jongseo <jongseo0703@naver.com>
2024-11-03 13:39:11 +0900

    스프링 프로젝트 생성

---
+++ backend/farmhelper/src/main/java/com/project/farmhelper/FarmhelperApplication.java
@@ -0,0 +1,13 @@
+package com.project.farmhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmhelperApplication.class, args);
+	}
+
+}
---
+++ backend/farmhelper/src/test/java/com/project/farmhelper/FarmhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.farmhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
