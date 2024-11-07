585d754cd74df434258256d0206bbbaf75ed77a9
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-07 13:27:51 +0900

    refactor: HomeFragment가 BaseFragment 상속 받도록 수정

--- frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/home/HomeFragment.kt
+++ frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/home/HomeFragment.kt
@@ -1,17 +1,16 @@
 package com.mjc.lst1995.farmhelper.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.view.GravityCompat
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.mjc.lst1995.farmhelper.R
+import com.mjc.lst1995.farmhelper.core.ui.BaseFragment
 import com.mjc.lst1995.farmhelper.core.ui.WeatherHomeAdapter
 import com.mjc.lst1995.farmhelper.core.ui.model.Weather
 import com.mjc.lst1995.farmhelper.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val weathers =
         listOf(
             Weather("맑음"),
@@ -30,13 +29,8 @@ class HomeFragment : Fragment() {
 
     private lateinit var adapter: WeatherHomeAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        // Inflate the layout for this fragment
-        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         binding.materialToolbar.setOnMenuItemClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.END)
@@ -47,9 +41,9 @@ class HomeFragment : Fragment() {
         adapter.submitList(weathers)
         binding.run {
             weathersRV.adapter = adapter
-            weathersRV.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            weathersRV.layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         }
-
-        return binding.root
     }
+
 }
