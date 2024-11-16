f9c28e85b0796167ab84888019349443200ba9da
SNXWXH <snxwxh@gmail.com>
2024-11-16 22:29:27 +0900

    Design: 헤더 컴포넌트 생성

--- frontend/web/src/app/layout.tsx
+++ frontend/web/src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Header from '@/components/Header';
 
 export const metadata: Metadata = {
   title: '농부의 하루',
   description: 'AI를 활용한 농사',
+  icons: {
+    icon: '/faviconLogo.png',
+  },
 };
 
 export default function RootLayout({
@@ -14,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className='font-["NanumSquareNeo"] flex flex-col min-h-screen bg-[#F9FDF4] text-black'>
+        <Header />
         {children}
       </body>
     </html>
---
+++ frontend/web/src/components/Header.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <>
+      <div className='flex justify-between bg-[#2B590F] text-white h-14 p-3'>
+        <Link href='/'>
+          <Image
+            src='/HeaderLogo.png'
+            alt='HeaderLogo'
+            width={110}
+            height={0}
+          />
+        </Link>
+        <div className='flex w-3/5 justify-between text-xl font-extrabold'>
+          <Link
+            href='/weather'
+            className='w-24 flex justify-center hover:text-[#669248]'
+          >
+            날씨
+          </Link>
+          <Link
+            href='/work'
+            className='w-24 flex justify-center hover:text-[#669248]'
+          >
+            작업일지
+          </Link>
+          <Link
+            href='/mypage'
+            className='w-24 flex justify-center hover:text-[#669248] '
+          >
+            마이페이지
+          </Link>
+        </div>
+        <Link href='/login'>
+          <Image
+            src='/login.png'
+            alt='Login'
+            width={35}
+            height={0}
+            className='ml-10'
+          />
+        </Link>
+      </div>
+    </>
+  );
+}
