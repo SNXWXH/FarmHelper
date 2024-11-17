c8f10a04e559341540e1f65600489acc4963ab88
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-18 07:34:48 +0900

    refactor: 코드 포맷팅 수정

--- frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/login/LoginFragment.kt
+++ frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/login/LoginFragment.kt
@@ -1,11 +1,7 @@
 package com.mjc.lst1995.farmhelper.login
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.mjc.lst1995.farmhelper.R
 import com.mjc.lst1995.farmhelper.core.ui.BaseFragment
 import com.mjc.lst1995.farmhelper.databinding.FragmentLoginBinding
@@ -13,9 +9,10 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 }
