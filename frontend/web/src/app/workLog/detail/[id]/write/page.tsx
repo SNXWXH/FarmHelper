e4825cc51ffd2303dc7f819bc7502bfd1317d133
SNXWXH <snxwxh@gmail.com>
2024-11-18 22:40:44 +0900

    design: 작업일지 수정 컴포넌트 생성

---
+++ frontend/web/src/app/workLog/detail/[id]/write/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useState } from 'react';
+import TodoDetail from '@/components/TodoDetail';
+
+export default function DetailWrite() {
+  return (
+    <div className='flex flex-col items-center h-screen w-full pt-14'>
+      <div className='w-3/5'>
+        <p className='mt-14 font-nanumHeavy font-heavy text-2xl'>
+          이설아님의 작업일지 {'>'} 감자 {'>'} 작업일지 수정
+        </p>
+        <div className='flex flex-col gap-5 w-full h-3/5 overflow-y-auto my-7'>
+          {details.map((detail, index) => (
+            <TodoDetail
+              key={index}
+              detail={detail}
+              onRemove={() => removeDetail(index)}
+            />
+          ))}
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex w-full items-center gap-3'>
+            <input
+              type='text'
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyPress={handleKeyPress}
+              placeholder='추가로 할 작업을 선택해주세요.'
+              className='flex-1 h-12 rounded-lg bg-gray-100 pl-3'
+            />
+            <button
+              onClick={addDetail}
+              className='px-4 py-2 rounded-md font-extrabold bg-[#698A54] text-white'
+            >
+              추가
+            </button>
+          </div>
+          <button className='mt-5 rounded-md font-extrabold w-24 h-9 bg-[#698A54] text-white'>
+            완료
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
