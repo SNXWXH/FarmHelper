c4a8b5ecb3b9abd5e95e9bd7c555a0ac2d13f4fc
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-20 23:37:34 +0900

    feat: 닉네임 설정화면 구현

---
+++ frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/login/NickNameFragment.kt
@@ -0,0 +1,26 @@
+package com.mjc.lst1995.farmhelper.login
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.mjc.lst1995.farmhelper.R
+import com.mjc.lst1995.farmhelper.core.ui.BaseFragment
+import com.mjc.lst1995.farmhelper.databinding.FragmentNickNameBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NickNameFragment : BaseFragment<FragmentNickNameBinding>(R.layout.fragment_nick_name) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        setButtonClickListener()
+    }
+
+    private fun setButtonClickListener() {
+        binding.nickNamePickBT.setOnClickListener {
+            findNavController().navigate(R.id.action_nickNameFragment_to_homeFragment)
+        }
+    }
+}
