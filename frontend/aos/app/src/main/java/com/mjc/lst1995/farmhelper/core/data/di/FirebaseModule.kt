4c4bf6b03af655619ada3de13cd2e21276bc1f58
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-20 15:32:49 +0900

    feat(data): firebaseAuth 모듈 설정

---
+++ frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/core/data/di/FirebaseModule.kt
@@ -0,0 +1,18 @@
+package com.mjc.lst1995.farmhelper.core.data.di
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class FirebaseModule {
+    @Provides
+    @Singleton
+    fun providesFirebaseAuth(): FirebaseAuth = Firebase.auth
+}
