bce17b51500cf3918baf9ef5c4c713b893a236a5
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-28 06:41:48 +0900

    feat(design): 작업 일지 생성 디자인 구현

---
+++ frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/feature/work/WorkCreateFragment.kt
@@ -0,0 +1,16 @@
+package com.mjc.lst1995.farmhelper.feature.work
+
+import android.os.Bundle
+import android.view.View
+import com.mjc.lst1995.farmhelper.R
+import com.mjc.lst1995.farmhelper.core.ui.BaseFragment
+import com.mjc.lst1995.farmhelper.databinding.FragmentWorkCreateBinding
+
+class WorkCreateFragment : BaseFragment<FragmentWorkCreateBinding>(R.layout.fragment_work_create) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
