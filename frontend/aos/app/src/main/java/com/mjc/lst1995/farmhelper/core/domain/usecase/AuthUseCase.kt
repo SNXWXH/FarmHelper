ff261ec0bca8a78d696f892679839b91fe30eff4
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-28 19:39:11 +0900

    feat(domain): 닉네임 설정 기능 구현

--- frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/core/domain/usecase/AuthUseCase.kt
+++ frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/core/domain/usecase/AuthUseCase.kt
@@ -3,8 +3,10 @@ package com.mjc.lst1995.farmhelper.core.domain.usecase
 import com.mjc.lst1995.farmhelper.core.domain.repository.AuthRepository
 import javax.inject.Inject
 
-class AuthUseCase
-    @Inject
-    constructor(
-        private val authRepository: AuthRepository,
-    )
+class AuthUseCase @Inject constructor(
+    private val authRepository: AuthRepository,
+) {
+    suspend fun userIsJoined() = authRepository.userIsJoined()
+
+    suspend fun setUserNickName(nickName: String) = authRepository.setUserNickName(nickName)
+}
