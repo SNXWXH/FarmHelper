31a35c1b6ad02e44126c359f14de22592227216b
SNXWXH <snxwxh@gmail.com>
2024-11-29 20:57:42 +0900

    feat: 이번달 인기 작물 API 데이터 출력

--- frontend/web/src/components/MonthRank.tsx
+++ frontend/web/src/components/MonthRank.tsx
@@ -1,27 +1,48 @@
 export default async function MonthRank() {
-  // const bestCrop: Response = await fetch(
-  //   `${process.env.BASE_URL}/api/bestCrop`
-  // );
-  // const bestCropData = await bestCrop.json();
-  // console.log('🚀  bestCropData:', bestCropData);
+  const bestCrop: Response = await fetch(
+    `${process.env.BASE_URL}/api/bestCrop`
+  );
+  const bestCropData = await bestCrop.json();
 
-  const ranks = [];
-  for (let idx = 1; idx <= 5; idx++) {
-    ranks.push(
-      <div key={idx} className='flex m-2'>
-        <span className='w-1/4 text-center font-extrabold text-xl'>{idx}</span>
-        <span className='flex justify-center items-center w-3/4 text-center text-xl'>
-          감자
-        </span>
-      </div>
-    );
-  }
+  const cropsToDisplay = [
+    ...bestCropData,
+    ...Array(10 - bestCropData.length).fill({
+      cropName: '데이터 없음',
+      count: 1,
+    }),
+  ];
+
+  const leftSideCrops = cropsToDisplay.slice(0, 5);
+  const rightSideCrops = cropsToDisplay.slice(5, 10);
 
   return (
     <>
       <div className='flex mt-8'>
-        <div className='w-1/2 flex flex-col'>{ranks}</div>
-        <div className='w-1/2 flex flex-col'>{ranks}</div>
+        <div className='w-1/2 flex flex-col'>
+          {leftSideCrops.map((crop, idx) => (
+            <div key={idx} className='flex m-2'>
+              <span className='w-1/4 text-center font-extrabold text-xl'>
+                {idx + 1}
+              </span>
+              <span className='flex justify-center items-center w-3/4 text-center text-xl'>
+                {crop?.cropName}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        <div className='w-1/2 flex flex-col'>
+          {rightSideCrops.map((crop, idx) => (
+            <div key={idx + 5} className='flex m-2'>
+              <span className='w-1/4 text-center font-extrabold text-xl'>
+                {idx + 6}
+              </span>
+              <span className='flex justify-center items-center w-3/4 text-center text-xl'>
+                {crop?.cropName}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
