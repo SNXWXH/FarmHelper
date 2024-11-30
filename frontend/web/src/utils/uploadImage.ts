9336ad118a1be35aed5d4cf7a8676c41b4f6d776
SNXWXH <snxwxh@gmail.com>
2024-11-30 17:58:35 +0900

    style: 주석 제거

--- frontend/web/src/utils/uploadImage.ts
+++ frontend/web/src/utils/uploadImage.ts
@@ -2,19 +2,16 @@ import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage } from '../../firebase-config';
 
 const uploadImage = async (file, userID) => {
-  console.log('🚀  userID:', userID);
-
   if (!file) throw new Error('No file selected.');
   if (!userID) throw new Error('User ID not found.');
 
   try {
-    // 파일명에서 확장자 제거
     const fileNameWithoutExtension = file.name
       .split('.')
       .slice(0, -1)
       .join('.');
-    const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); // 파일명에 사용할 타임스탬프
-    const filePath = `${userID}/${fileNameWithoutExtension}-${timestamp}.jpg`; // 사용자 ID 폴더 아래에 파일 경로 생성
+    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+    const filePath = `${userID}/${fileNameWithoutExtension}-${timestamp}.jpg`;
 
     const fileRef = ref(storage, filePath);
     await uploadBytes(fileRef, file);
