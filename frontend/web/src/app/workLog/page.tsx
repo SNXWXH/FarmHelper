7ab7c0a4ab9d176e7e28b3f1eba2b7cab3536e0a
SNXWXH <snxwxh@gmail.com>
2024-12-01 17:08:06 +0900

    feat: 작업일지 리스트 API 닉네임 추가

--- frontend/web/src/app/workLog/page.tsx
+++ frontend/web/src/app/workLog/page.tsx
@@ -4,8 +4,10 @@ import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import CropList from '@/components/CropList';
 import CropListCard from '@/components/CropListCard';
+import Link from 'next/link';
 
 export default function WorkLog() {
+  const [nickName, setNickName] = useState('');
   const [isGridView, setIsGridView] = useState(true);
   const [cropList, setCropList] = useState<any[]>([]);
   const { data: session } = useSession();
@@ -19,6 +21,7 @@ export default function WorkLog() {
           );
           const data = await response.json();
           setCropList(data.workList);
+          setNickName(data.nickname);
         } catch (error) {
           console.error('Error fetching crop data:', error);
         }
@@ -33,7 +36,7 @@ export default function WorkLog() {
       <div className='flex flex-col items-center h-screen w-full pt-14'>
         <div className='w-3/5'>
           <p className='mt-14 font-nanumHeavy font-heavy text-2xl'>
-            이설아님의 작업일지
+            {nickName}님의 작업일지
           </p>
           <div className='flex justify-end mt-6 mb-9'>
             <button
@@ -54,25 +57,34 @@ export default function WorkLog() {
             {isGridView ? (
               cropList.length > 0 ? (
                 cropList.map((crop, idx) => (
-                  <CropListCard
+                  <Link href={`workLog/detail/${crop.cropId}`} key={idx}>
+                    <CropListCard
+                      key={idx}
+                      cropName={crop.cropName}
+                      date={crop.cropDate}
+                      imageUrl={decodeURIComponent(crop.imageUrl)}
+                    />
+                  </Link>
+                ))
+              ) : (
+                <p className='flex justify-center items-center h-screen'>
+                  작업일지가 없습니다.
+                </p>
+              )
+            ) : cropList.length > 0 ? (
+              cropList.map((crop, idx) => (
+                <Link href={`workLog/detail/${idx}`} key={idx}>
+                  <CropList
                     key={idx}
                     cropName={crop.cropName}
                     date={crop.cropDate}
                   />
-                ))
-              ) : (
-                <p>작업일지가 없습니다.</p>
-              )
-            ) : cropList.length > 0 ? (
-              cropList.map((crop, idx) => (
-                <CropList
-                  key={idx}
-                  cropName={crop.cropName}
-                  date={crop.cropDate}
-                />
+                </Link>
               ))
             ) : (
-              <p>작업일지가 없습니다.</p>
+              <p className='flex justify-center items-center w-full h-screen'>
+                작업일지가 없습니다.
+              </p>
             )}
           </div>
         </div>
