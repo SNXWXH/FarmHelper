585f070327bead90d83aa534f57ebd6a96c31d5e
SNXWXH <snxwxh@gmail.com>
2024-12-01 17:11:33 +0900

    design: 작업일지 리스트 CSS 수정

--- frontend/web/src/components/CropList.tsx
+++ frontend/web/src/components/CropList.tsx
@@ -8,9 +8,9 @@ export default function CropList({
   return (
     <>
       <div className='flex justify-between items-center h-16 rounded-3xl font-bold text-xl bg-[#F4FEE6]'>
-        <p className='w-2/3 ml-6 font-nanumHeavy font-heavy'>{cropName}</p>
-        <div className='flex'>
-          <p className='text-[#585858]'>{date}</p>
+        <p className='w-3/4 ml-6 font-nanumHeavy font-heavy'>{cropName}</p>
+        <div className='flex '>
+          <p className='text-[#585858] w-44'>{date}</p>
           <p className='w-1/3 mx-6 items-end'>{'>'}</p>
         </div>
       </div>
