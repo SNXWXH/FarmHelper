d5db9cfe85cd4ac7a8ade64f8e3aeefe716414cb
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-12-03 01:07:41 +0900

    feat: 파이어베이스 스토리지 모듈 추가

--- frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/core/data/di/FirebaseModule.kt
+++ frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/core/data/di/FirebaseModule.kt
@@ -3,6 +3,8 @@ package com.mjc.lst1995.farmhelper.core.data.di
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,4 +17,8 @@ class FirebaseModule {
     @Provides
     @Singleton
     fun providesFirebaseAuth(): FirebaseAuth = Firebase.auth
+
+    @Provides
+    @Singleton
+    fun providesFirebaseStorage(): FirebaseStorage = Firebase.storage
 }
