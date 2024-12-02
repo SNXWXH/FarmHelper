15b49911419b1234e74c2458f1cf6fea98643744
SNXWXH <snxwxh@gmail.com>
2024-12-02 15:13:53 +0900

    feat: 이미지가 null인 경우 기본 사진 보이기

--- frontend/web/src/components/CropListCard.tsx
+++ frontend/web/src/components/CropListCard.tsx
@@ -13,7 +13,13 @@ export default function CropListCard({
     <>
       <div className='flex flex-col items-center bg-[#F4FEE6] w-64 h-72'>
         <div className='h-3/5 flex justify-center items-center mt-4'>
-          <Image src={imageUrl} alt='img' width={140} height={0} unoptimized />
+          <Image
+            src={imageUrl === 'null' ? '/faviconLogo.png' : imageUrl}
+            alt='img'
+            width={140}
+            height={0}
+            unoptimized
+          />
         </div>
         <div className='flex flex-col items-center justify-center h-2/5 gap-3'>
           <p className='text-xl text-[#585858]'>{date}</p>
