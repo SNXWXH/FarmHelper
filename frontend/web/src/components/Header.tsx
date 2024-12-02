9f8076975875536df9f67c0f9c9af02f2d85c6e2
SNXWXH <snxwxh@gmail.com>
2024-12-02 18:25:15 +0900

    feat: 헤더 수정 및 토글 구현

--- frontend/web/src/components/Header.tsx
+++ frontend/web/src/components/Header.tsx
@@ -1,48 +1,45 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { data: session } = useSession();
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
-    <>
-      <div className='fixed top-0 left-0 flex justify-between bg-[#2B590F] text-white h-14 p-3 w-full font-nanumHeavy font-heavy'>
-        <Link href='/'>
-          <Image
-            src='/HeaderLogo.png'
-            alt='HeaderLogo'
-            width={110}
-            height={0}
-          />
-        </Link>
-        <div className='flex w-3/5 justify-between text-lg font-extrabold'>
-          <Link
-            href='/weather'
-            className='w-24 flex justify-center hover:text-[#669248]'
-          >
-            날씨
-          </Link>
-          <Link
-            href='/workLog'
-            className='w-24 flex justify-center hover:text-[#669248]'
-          >
-            작업일지
-          </Link>
-          <Link
-            href='/mypage'
-            className='w-24 flex justify-center hover:text-[#669248] '
-          >
-            마이페이지
-          </Link>
-        </div>
-        <Link href='/login'>
-          <Image
-            src='/login.png'
-            alt='Login'
-            width={35}
-            height={0}
-            className='ml-10'
-          />
-        </Link>
+    <div className='fixed top-0 left-0 flex justify-between bg-[#2B590F] text-white h-14 p-3 w-full font-nanumHeavy font-heavy'>
+      <Link href='/'>
+        <Image src='/HeaderLogo.png' alt='HeaderLogo' width={110} height={0} />
+      </Link>
+
+      <div className='relative'>
+        <Image
+          src='/login.png'
+          alt='Login'
+          width={35}
+          height={0}
+          className='ml-10 cursor-pointer'
+          onClick={toggleMenu}
+        />
+        {isMenuOpen && (
+          <div className='absolute right-0 bg-white text-black p-4 rounded-lg shadow-lg w-32'>
+            <Link href='/login' className='block px-4 py-2 hover:bg-gray-200'>
+              {session ? '로그아웃' : '로그인'}
+            </Link>
+
+            <Link href='/workLog' className='block px-4 py-2 hover:bg-gray-200'>
+              작업일지
+            </Link>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
