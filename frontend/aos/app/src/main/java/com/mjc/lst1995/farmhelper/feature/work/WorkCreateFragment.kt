a1274f0135c33ef48e282498a9ba6eb88e3d37a4
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-12-03 00:59:14 +0900

    feat: 커버 이미지 선택 전 권환 확인 기능 구현

--- frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/feature/work/WorkCreateFragment.kt
+++ frontend/aos/app/src/main/java/com/mjc/lst1995/farmhelper/feature/work/WorkCreateFragment.kt
@@ -1,16 +1,51 @@
 package com.mjc.lst1995.farmhelper.feature.work
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.load
+import com.mjc.lst1995.farmhelper.MainActivity
 import com.mjc.lst1995.farmhelper.R
 import com.mjc.lst1995.farmhelper.core.ui.BaseFragment
 import com.mjc.lst1995.farmhelper.databinding.FragmentWorkCreateBinding
 
 class WorkCreateFragment : BaseFragment<FragmentWorkCreateBinding>(R.layout.fragment_work_create) {
+    private lateinit var activity: MainActivity
+
+    private val pickImageLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let {
+                binding.coverImageIV.load(it) {
+                    crossfade(true)
+                }
+            }
+        }
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        pickImage()
+    }
+
+    private fun pickImage() {
+        binding.coverImageIV.setOnClickListener {
+            if (!permissionCheck()) {
+                requestPermission()
+                return@setOnClickListener
+            }
+            pickImageLauncher.launch("image/*")
+        }
+    }
+
+    private fun permissionCheck(): Boolean {
+        activity = requireActivity() as MainActivity
+        return activity.permissionCheck()
+    }
+
+    private fun requestPermission() {
+        activity.requestPermission()
     }
 }
