8355058890e96ee65f1c4a8e431a7cbe2e0c65ac
SNXWXH <snxwxh@gmail.com>
2024-12-02 14:20:33 +0900

    refactor: 컴포넌트 분리 및 닉네임, 작물 데이터 연동

--- frontend/web/src/app/workLog/detail/[id]/page.tsx
+++ frontend/web/src/app/workLog/detail/[id]/page.tsx
@@ -1,36 +1,31 @@
 'use client';
 
 import AllDetail from '@/components/AllDetail';
-import DeatilList from '@/components/DeatilList';
 import TodayDetail from '@/components/TodayDetail';
 import { useSession } from 'next-auth/react';
-import { useParams } from 'next/navigation';
+import { useParams, useSearchParams } from 'next/navigation';
 
 export default function Detail() {
   const { data: session } = useSession();
   const { id } = useParams();
+
+  const searchParams = useSearchParams();
+
+  const userId = searchParams.get('nickName');
+  const cropName = searchParams.get('cropName');
 
   return (
     <>
       <div className='flex flex-col items-center h-screen w-full pt-14'>
         <div className='w-3/5'>
           <p className='mt-14 font-nanumHeavy font-heavy text-2xl'>
-            이설아님의 작업일지 {'>'} 감자
+            {userId}님의 작업일지 {'>'} {cropName}
           </p>
           <div className='mt-5'>
             <TodayDetail userId={session?.user.uid} cropId={id} />
           </div>
-          <div className='w-full h-0.5 bg-black my-6'> </div>
-          <div className='flex flex-col'>
-            <div className='flex justify-end'>
-              <button className='w-28 h-9 bg-[#B3D99C] text-white rounded-xl mb-6'>
-                내림차순
-              </button>
-            </div>
-            <div className='flex flex-col mb-8'>
-              <AllDetail />
-            </div>
-          </div>
+
+          <AllDetail userId={session?.user.uid} cropId={id} />
         </div>
       </div>
     </>
