36d5bab24d1cd8353d96063d20567efee704c3a9
SNXWXH <snxwxh@gmail.com>
2024-12-05 00:27:26 +0900

    design: 작업일지 디테일 페이지 스켈레톤 적용

--- frontend/web/src/app/workLog/detail/[id]/page.tsx
+++ frontend/web/src/app/workLog/detail/[id]/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
+import { Suspense } from 'react';
 import AllDetail from '@/components/AllDetail';
 import TodayDetail from '@/components/TodayDetail';
 import { useSession } from 'next-auth/react';
 import { useParams, useSearchParams } from 'next/navigation';
+import Skeleton from '@/components/Skeleton';
 
 export default function Detail() {
   const { data: session } = useSession();
   const { id } = useParams();
 
   const searchParams = useSearchParams();
-
   const userId = searchParams.get('nickName');
   const cropName = searchParams.get('cropName');
   const cropDate = searchParams.get('cropDate');
@@ -22,19 +23,24 @@ export default function Detail() {
           <p className='mt-14 font-nanumHeavy font-heavy text-2xl'>
             {userId}님의 작업일지 {'>'} {cropName}
           </p>
+
           <div className='mt-5'>
-            <TodayDetail
+            <Suspense fallback={<Skeleton />}>
+              <TodayDetail
+                userId={session?.user.uid}
+                cropId={id}
+                cropDate={cropDate}
+              />
+            </Suspense>
+          </div>
+
+          <Suspense fallback={<Skeleton />}>
+            <AllDetail
               userId={session?.user.uid}
               cropId={id}
               cropDate={cropDate}
             />
-          </div>
-
-          <AllDetail
-            userId={session?.user.uid}
-            cropId={id}
-            cropDate={cropDate}
-          />
+          </Suspense>
         </div>
       </div>
     </>
