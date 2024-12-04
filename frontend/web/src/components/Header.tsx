93ff80affc7a94f71f972e499f0dce3f539f9c63
SNXWXH <snxwxh@gmail.com>
2024-12-05 00:22:46 +0900

    refactor: 아이콘 변경 및 라우터 로직 추가

--- frontend/web/src/components/Header.tsx
+++ frontend/web/src/components/Header.tsx
@@ -4,13 +4,25 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { data: session } = useSession();
+  const router = useRouter();
 
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
+  };
+
+  const handleWorkLogClick = () => {
+    if (!session) {
+      alert('로그인 후 이용이 가능합니다.');
+      toggleMenu();
+      router.push('/login');
+    } else {
+      toggleMenu();
+    }
   };
 
   return (
@@ -21,7 +33,7 @@ export default function Header() {
 
       <div className='relative'>
         <Image
-          src='/login.png'
+          src='/bars.png'
           alt='Login'
           width={35}
           height={0}
@@ -30,13 +42,28 @@ export default function Header() {
         />
         {isMenuOpen && (
           <div className='absolute right-0 bg-white text-black p-4 rounded-lg shadow-lg w-32'>
-            <Link href='/login' className='block px-4 py-2 hover:bg-gray-200'>
-              {session ? '로그아웃' : '로그인'}
-            </Link>
-
-            <Link href='/workLog' className='block px-4 py-2 hover:bg-gray-200'>
-              작업일지
-            </Link>
+            <div onClick={toggleMenu}>
+              <Link href='/login' className='block px-4 py-2 hover:bg-gray-200'>
+                {session ? '로그아웃' : '로그인'}
+              </Link>
+            </div>
+            {session && (
+              <Link
+                href='/workLog'
+                className='block px-4 py-2 hover:bg-gray-200'
+                onClick={toggleMenu}
+              >
+                작업일지
+              </Link>
+            )}
+            {!session && (
+              <div
+                className='block px-4 py-2 hover:bg-gray-200 cursor-pointer'
+                onClick={handleWorkLogClick}
+              >
+                작업일지
+              </div>
+            )}
           </div>
         )}
       </div>
